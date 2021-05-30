f4c8a0a6b7248bce75c02cbe623b013a676427cc
Róger Ordóñez <roger.ordez@hotmail.com>
2021-05-29 22:21:12 -0600

    feat: add new badges for stats

--- src/features/InfoSection/index.tsx
+++ src/features/InfoSection/index.tsx
@@ -18,22 +18,64 @@ export const InfoSection: FC = () => {
   return (
     <div
       css={[
-        tw`flex flex-col justify-between p-4 bg-gray-900 w-full lg:w-1/3`,
+        tw`flex flex-col justify-between  p-4 bg-gray-900 w-full lg:w-1/3`,
         isDesktop && { minHeight: "10rem" },
         !isDesktop && { minHeight: "8rem" },
       ]}
     >
       <div css={tw`flex flex-col lg:items-center`}>
-        <div
-          css={tw`text-gray-400 uppercase tracking-wider text-sm lg:text-lg`}
-        >
-          verb
+        <div css={tw`flex items-center justify-center mb-2 lg:flex-col`}>
+          <div css={tw`flex mb-0 lg:mb-8`}>
+            <div css={tw`text-gray-200`}>
+              <span
+                css={tw`pt-0.5 pb-0.5 text-sm lg:text-xs px-2 mr-1 bg-green-200 text-gray-800 rounded-full`}
+              >
+                Success: 999
+              </span>
+            </div>
+            <div css={tw`text-gray-200`}>
+              <span
+                css={tw`pt-0.5 pb-0.5 text-sm lg:text-xs px-2 mr-1 bg-red-200 text-gray-800 rounded-full`}
+              >
+                Error: 999
+              </span>
+            </div>
+            <div css={tw`text-gray-200`}>
+              <span
+                css={tw`pt-0.5 pb-0.5 text-sm lg:text-xs px-2 mr-1 bg-yellow-200 text-gray-800 rounded-full`}
+              >
+                Skipped: 999
+              </span>
+            </div>
+          </div>
+          <div
+            css={tw`text-gray-400 uppercase tracking-wider text-lg hidden lg:block`}
+          >
+            verb
+          </div>
         </div>
-        <div css={tw`text-white text-3xl lg:text-4xl mt-2 lg:mt-4`}>
-          {currentVerb.verbTense?.tenses.infinitive}
+        <div css={tw`flex justify-between lg:justify-center lg:flex-col`}>
+          <div
+            css={tw`text-white text-center text-3xl lg:text-4xl mt-2 lg:mt-4`}
+          >
+            {currentVerb.verbTense?.tenses.infinitive}
+          </div>
+          <div css={tw`lg:pt-5`}>
+            <button
+              css={[
+                currentVerb.isVerbChecked &&
+                  tw`px-10 py-1 lg:px-16 lg:py-2 lg:mb-8  bg-gray-100 text-gray-900 text-lg rounded-full shadow-md hover:bg-gray-300 focus:outline-none`,
+                !currentVerb.isVerbChecked && tw`hidden`,
+                currentVerb.isVerbCorrect && tw`hidden`,
+              ]}
+              type="button"
+              onClick={showAnswer}
+            >
+              Skip verb
+            </button>
+          </div>
         </div>
       </div>
-
       <div
         css={tw`flex flex-wrap lg:flex-col-reverse justify-between items-end lg:items-center`}
       >
@@ -52,30 +94,6 @@ export const InfoSection: FC = () => {
           </div>
         </Link>
         <div>
-          {/* <div css={tw`text-gray-200 mt-4`}>
-            Errors:{" "}
-            <span
-              css={tw`pt-0.5 pb-0.5 text-sm px-3 bg-red-200 text-gray-800 rounded-full`}
-            >
-              5
-            </span>
-          </div>
-          <div css={tw`text-gray-200 mt-4`}>
-            Success:{" "}
-            <span
-              css={tw`pt-0.5 pb-0.5 text-sm px-3 bg-green-200 text-gray-800 rounded-full`}
-            >
-              3
-            </span>
-          </div>
-          <div css={tw`text-gray-200 mt-4`}>
-            Skipped:{" "}
-            <span
-              css={tw`pt-0.5 pb-0.5 text-sm px-3 bg-yellow-200 text-gray-800 rounded-full`}
-            >
-              2
-            </span>
-          </div> */}
           <div>
             <button
               css={[
